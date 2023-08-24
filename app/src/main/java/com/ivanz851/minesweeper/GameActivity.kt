e48ee25019d16c6db9289235a2ca44fec04f0e55
Ivan Zakharov <iazakharov_3@edu.hse.ru>
2023-08-24 16:43:57 +0300

    Added Minesweeper game activity

---
+++ app/src/main/java/com/ivanz851/minesweeper/GameActivity.kt
@@ -0,0 +1,12 @@
+package com.ivanz851.minesweeper
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ivanz851.minesweeper.R
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
